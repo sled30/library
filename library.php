de41731e23707dde8f503de6a646e1db2234e9f0
sled <max-30@mail.ru>
2018-08-01 22:44:36 +0400

    library

--- library.php
+++ library.php
@@ -32,43 +32,48 @@ if(!isset($_SESSION['role']))
     <h1>Добро пожаловать</h1>
     <!-- Заголовок -->
     <!-- Область основного контента -->
+    <!-- Область основного контента -->
    <h3>книги на руках</h3>
    <table>
 	   <tr><td>Автор (фамилия)</td><td>Автор (имя)
 		  </td><td>Название книги</td><td>дата выдачи</td></tr>
 	   
    <?php
-   $users=$_SESSION["id"]; /*прилитает какого пользователя смотреть*/
+   $users=$_SESSION["id"]; /*прилетает какого пользователя смотреть*/
    $selbook="SELECT avtor.lastname as avtorlastname, avtor.name as avtorname, books.name as bookname, book_vidan.createdate as createdate, users.id, users.name as username 
    FROM book_vidan JOIN users ON users.id=book_vidan.userid
-						 JOIN books on books.id=book_vidan.bookid
-                         join avtor on books.avtor=avtor.id where users.id='$users'";
-         $fromuser=mysqli_query($connect, $selbook);
-        while($bookdb=mysqli_fetch_assoc($fromuser))
-      {
-		  echo "<tr><td>".$bookdb['avtorlastname']."</td><td>".$bookdb['avtorname'].
-		  "</td><td>".$bookdb['bookname']."</td><td>".$bookdb['createdate']."</td></tr>";
-		}
+   JOIN books on books.id=book_vidan.bookid
+   join avtor on books.avtor=avtor.id where users.id='$users'";
+   $fromuser=mysqli_query($connect, $selbook);
+   while($bookdb=mysqli_fetch_assoc($fromuser))
+   {
+   echo "<tr><td>".$bookdb['avtorlastname']."</td><td>".$bookdb['avtorname'].
+   "</td><td>".$bookdb['bookname']."</td><td>".$bookdb['createdate']."</td></tr>";
+   }
+   if(!isset($_SESSION['role'])==1)
+   {
+   
+   $sqladminotcet="SELECT avtor.lastname as avtorlastname, avtor.name as avtorname, books.name as bookname, book_vidan.createdate as createdate, users.id, users.name as username 
+   FROM book_vidan JOIN users ON users.id=book_vidan.userid
+   JOIN books on books.id=book_vidan.bookid
+   join avtor on books.avtor=avtor.id";
+   echo $sqladminotcet;
+   $adminotcetdb=mysqli_query($connect, $sqladminotcet);
+   
+   while($adminotcet= mysqli_fetch_assoc($adminotcetdb))
+   //echo "rr";
+   echo $adminotcet['avtorlastname']. $adminotcet['avtornamm']. $adminotcet['bookname'].$adminotcet['createdate'].$adminotcet['username'] ;               
+   
+}
+
    ?>
-   </table>
     <!-- Область основного контента -->
   </div>
   <div id="nav">
     <!-- Навигация -->
     <h2>Навигация по сайту</h2>
     <!-- Меню -->
-   <ul>
-      <li><a href='index.php'>Домой</a>
-      </li>
-      <li><a href='menu.php'>Управление книгами</a>
-      </li>
-      <li><a href='useradd.php'>Управление пользователями</a>
-      </li>
-      <li><a href='getbook.php'>выдача книг</a>
-      </li>
-        <li><a href='library.php'>выданые книг</a>
-      </li>
-    </ul>
+   <?php require_once 'conf/leftconsol.php'?>
     <!-- Меню -->
     <!-- Навигация -->
   </div>
